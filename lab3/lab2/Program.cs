771cd801c8823f8ebbe2813fcc9c3d853c2c237a
k.mcdonald2 <k.mcdonald2@SHL125-08>
2019-10-03 15:17:42 -0700

    Thursday 10-3 demo from class

---
+++ lab3/lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportsCar myCar = new SportsCar();
+            myCar.PetName = "my beatup sienna";
+            Console.WriteLine("My car: \"{0}\",\t{1}", myCar.getPetName(), myCar.PetName);
+        }
+    }
+}
